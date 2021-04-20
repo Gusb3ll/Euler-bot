a45b743d6b1f0d214c545d57fae5ca4ce29fe921
Gusb3ll <Kitpipat.0647@gmail.com>
2021-04-20 12:23:03 +0700

    Make message deletion timing the same across the bot

--- Euler.py
+++ Euler.py
@@ -42,128 +42,107 @@ async def on_message(message):
         await t.delete()
 
     if any(word in m for word in Commands.Info):
-        p1 = await message.channel.send('Created by Gusbell for POSN discord channel,')
-        await asyncio.sleep(1)
-        p2 = await message.channel.send('further information please visit https://github.com/Gusb3ll/Euler-bot')
+        t = await message.channel.send('Created by Gusbell for POSN discord channel, further more information please visit https://github.com/Gusb3ll/Euler-bot')
         await asyncio.sleep(20)
         await message.delete()
-        await p1.delete()
-        await p2.delete()
+        await t.delete()
 
 ### Real miracle begin here ###
 
-    if m.startswith('$Generate alg 1'):  # !
+    if m.startswith('$Generate alg 1'): #!
         t = await message.channel.send(file=discord.File(random.choice(Problems.Alg_Easy)))
-        await asyncio.sleep(1)
         await asyncio.sleep(30)
         await message.delete()
         await t.delete()
     elif m.startswith('$Generate alg 2'):
         t = await message.channel.send(file=discord.File(random.choice(Problems.Alg_Normal)))
-        await asyncio.sleep(1)
         await asyncio.sleep(30)
         await message.delete()
         await t.delete()
     elif m.startswith('$Generate alg 3'):
         t = await message.channel.send(file=discord.File(random.choice(Problems.Alg_Hard)))
-        await asyncio.sleep(1)
         await asyncio.sleep(30)
         await message.delete()
         await t.delete()
-    elif m.startswith('$Generate geo 1'):  # !
+    elif m.startswith('$Generate geo 1'): #!
         t = await message.channel.send(file=discord.File(random.choice(Problems.Geo_Easy)))
-        await asyncio.sleep(1)
         await asyncio.sleep(30)
         await message.delete()
         await t.delete()
     elif m.startswith('$Generate geo 2'):
         t = await message.channel.send(file=discord.File(random.choice(Problems.Geo_Normal)))
-        await asyncio.sleep(1)
         await asyncio.sleep(30)
         await message.delete()
         await t.delete()
     elif m.startswith('$Generate geo 3'):
         t = await message.channel.send(file=discord.File(random.choice(Problems.Geo_Hard)))
-        await asyncio.sleep(1)
         await asyncio.sleep(30)
         await message.delete()
         await t.delete()
-    elif m.startswith('$Generate com 1'):  # !
+    elif m.startswith('$Generate com 1'): #!
         t = await message.channel.send(file=discord.File(random.choice(Problems.Com_Easy)))
-        await asyncio.sleep(1)
         await asyncio.sleep(30)
         await message.delete()
         await t.delete()
     elif m.startswith('$Generate com 2'):
         t = await message.channel.send(file=discord.File(random.choice(Problems.Com_Normal)))
-        await asyncio.sleep(1)
         await asyncio.sleep(30)
         await message.delete()
         await t.delete()
     elif m.startswith('$Generate com 3'):
         t = await message.channel.send(file=discord.File(random.choice(Problems.Com_Hard)))
-        await asyncio.sleep(1)
         await asyncio.sleep(30)
         await message.delete()
         await t.delete()
-    elif m.startswith('$Generate iq 1'):  # !
+    elif m.startswith('$Generate iq 1'): #!
         t = await message.channel.send(file=discord.File(random.choice(Problems.iq_Easy)))
-        await asyncio.sleep(1)
         await asyncio.sleep(30)
         await message.delete()
         await t.delete()
     elif m.startswith('$Generate iq 2'):
         t = await message.channel.send(file=discord.File(random.choice(Problems.iq_Normal)))
-        await asyncio.sleep(1)
         await asyncio.sleep(30)
         await message.delete()
         await t.delete()
     elif m.startswith('$Generate iq 3'):
         t = await message.channel.send(file=discord.File(random.choice(Problems.iq_Hard)))
-        await asyncio.sleep(1)
         await asyncio.sleep(30)
         await message.delete()
         await t.delete()
-    elif m.startswith('$Generate num 1'):  # !
+    elif m.startswith('$Generate num 1'): #!
         t = await message.channel.send(file=discord.File(random.choice(Problems.num_Easy)))
-        await asyncio.sleep(1)
         await asyncio.sleep(30)
         await message.delete()
         await t.delete()
     elif m.startswith('$Generate num 2'):
         t = await message.channel.send(file=discord.File(random.choice(Problems.num_Normal)))
-        await asyncio.sleep(1)
         await asyncio.sleep(30)
         await message.delete()
         await t.delete()
     elif m.startswith('$Generate num 3'):
         t = await message.channel.send(file=discord.File(random.choice(Problems.num_Hard)))
-        await asyncio.sleep(1)
         await asyncio.sleep(30)
         await message.delete()
         await t.delete()
-    elif m.startswith('$Generate func 1'):  # !
+    elif m.startswith('$Generate func 1'): #!
         t = await message.channel.send(file=discord.File(random.choice(Problems.func_Easy)))
-        await asyncio.sleep(1)
         await asyncio.sleep(30)
         await message.delete()
         await t.delete()
     elif m.startswith('$Generate func 2'):
         t = await message.channel.send(file=discord.File(random.choice(Problems.func_Normal)))
-        await asyncio.sleep(1)
         await asyncio.sleep(30)
         await message.delete()
         await t.delete()
     elif m.startswith('$Generate func 3'):
         t = await message.channel.send(file=discord.File(random.choice(Problems.func_Hard)))
-        await asyncio.sleep(1)
         await asyncio.sleep(30)
         await message.delete()
         await t.delete()
     elif m.startswith('$Generate') or m.startswith('$generate'):
         p1 = await message.channel.send('Usage : ```$Generate [topic] [difficulty]```')
         await asyncio.sleep(1)
-        p2 = await message.channel.send('List of topic : ```alg : Algebra, geo : Geometry, com : Combinatoric, iq : Inequality, num : Number theory, func : Functional Equation```')
+        p2 = await message.channel.send('List of topic : ```alg - Algebra, geo - Geometry, com - Combinatoric, iq - Inequality, num - Number theory, func - Functional Equation```')
         await asyncio.sleep(1)
         p3 = await message.channel.send('List of difficulty : ```1 Easy, 2 Normal , 3 Hard```')
         await asyncio.sleep(20)
