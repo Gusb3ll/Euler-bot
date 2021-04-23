00de40d5c6ccd6ab011f61f6030a6986fb2bc671
Gusb3ll <Kitpipat.0647@gmail.com>
2021-04-23 16:41:20 +0700

    Added algebra problems

--- objects/Problems.py
+++ objects/Problems.py
@@ -1,23 +1,71 @@
-Alg_Easy = ['Problems/Algebra/Easy/test.jpg']
-Alg_Normal = ['Problems/Algebra/Normal/test.jpg']
-Alg_Hard = ['Problems/Algebra/Hard/test.jpg']
+Alg_Easy = ['Problems/Algebra/Easy/01.png', 'Problems/Algebra/Easy/02.png',
+            'Problems/Algebra/Easy/03.png', 'Problems/Algebra/Easy/04.png', 
+            'Problems/Algebra/Easy/05.png', 'Problems/Algebra/Easy/06.png', 
+            'Problems/Algebra/Easy/07.png', 'Problems/Algebra/Easy/08.png',
+            'Problems/Algebra/Easy/09.png', 'Problems/Algebra/Easy/10.png',
+            'Problems/Algebra/Easy/11.png', 'Problems/Algebra/Easy/12.png',
+            'Problems/Algebra/Easy/13.png', 'Problems/Algebra/Easy/14.png',
+            'Problems/Algebra/Easy/15.png', 'Problems/Algebra/Easy/16.png',
+            'Problems/Algebra/Easy/17.png', 'Problems/Algebra/Easy/18.png', 
+            'Problems/Algebra/Easy/19.png', 'Problems/Algebra/Easy/20.png', 
+            'Problems/Algebra/Easy/21.png', 'Problems/Algebra/Easy/22.png']
+
+Alg_Normal = ['Problems/Algebra/Normal/01.png', 'Problems/Algebra/Normal/02.png',
+            'Problems/Algebra/Normal/03.png', 'Problems/Algebra/Normal/04.png',
+            'Problems/Algebra/Normal/05.png', 'Problems/Algebra/Normal/06.png',
+            'Problems/Algebra/Normal/07.png', 'Problems/Algebra/Normal/08.png',
+            'Problems/Algebra/Normal/09.png', 'Problems/Algebra/Normal/10.png',
+            'Problems/Algebra/Normal/11.png', 'Problems/Algebra/Normal/12.png',
+            'Problems/Algebra/Normal/13.png', 'Problems/Algebra/Normal/14.png',
+            'Problems/Algebra/Normal/15.png', 'Problems/Algebra/Normal/16.png',
+            'Problems/Algebra/Normal/17.png', 'Problems/Algebra/Normal/18.png',
+            'Problems/Algebra/Normal/19.png', 'Problems/Algebra/Normal/20.png',
+            'Problems/Algebra/Normal/21.png', 'Problems/Algebra/Normal/22.png', 
+            'Problems/Algebra/Normal/23.png', 'Problems/Algebra/Normal/24.png', 
+            'Problems/Algebra/Normal/25.png']
+
+Alg_Hard = ['Problems/Algebra/Hard/01.png', 'Problems/Algebra/Hard/02.png',
+            'Problems/Algebra/Hard/03.png', 'Problems/Algebra/Hard/04.png',
+            'Problems/Algebra/Hard/05.png', 'Problems/Algebra/Hard/06.png',
+            'Problems/Algebra/Hard/07.png', 'Problems/Algebra/Hard/08.png',
+            'Problems/Algebra/Hard/09.png', 'Problems/Algebra/Hard/10.png',
+            'Problems/Algebra/Hard/11.png', 'Problems/Algebra/Hard/12.png',
+            'Problems/Algebra/Hard/13.png', 'Problems/Algebra/Hard/14.png',
+            'Problems/Algebra/Hard/15.png', 'Problems/Algebra/Hard/16.png',
+            'Problems/Algebra/Hard/17.png', 'Problems/Algebra/Hard/18.png',
+            'Problems/Algebra/Hard/19.png', 'Problems/Algebra/Hard/20.png',
+            'Problems/Algebra/Hard/21.png', 'Problems/Algebra/Hard/22.png', 
+            'Problems/Algebra/Hard/23.png', 'Problems/Algebra/Hard/24.png', 
+            'Problems/Algebra/Hard/25.png', 'Problems/Algebra/Hard/26.png']
+
+
 
 Geo_Easy = ['Problems/Geometry/Easy/test.jpg']
 Geo_Normal = ['Problems/Geometry/Normal/test.jpg']
 Geo_Hard = ['Problems/Geometry/Hard/test.jpg']
 
+
+
 Com_Easy = ['Problems/Combinatoric/Easy/test.jpg']
 Com_Normal = ['Problems/Combinatoric/Normal/test.jpg']
 Com_Hard = ['Problems/Combinatoric/Hard/test.jpg']
+
+
 
 iq_Easy = ['Problems/Inequality/Easy/test.jpg']
 iq_Normal = ['Problems/Inequality/Normal/test.jpg']
 iq_Hard = ['Problems/Inequality/Hard/test.jpg']
 
+
+
 num_Easy = ['Problems/NumberTheory/Easy/test.jpg']
 num_Normal = ['Problems/NumberTheory/Normal/test.jpg']
 num_Hard = ['Problems/NumberTheory/Hard/test.jpg']
 
+
+
 func_Easy = ['Problems/FunctionalEquation/Easy/test.jpg']
 func_Normal = ['Problems/FunctionalEquation/Normal/test.jpg']
 func_Hard = ['Problems/FunctionalEquation/Hard/test.jpg']
+
+
